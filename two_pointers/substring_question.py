8d3bb24de539b1d14f58ad52db7d3013a7cb713b
“Carlos <cmariscalaws@gmail.com>
2025-06-25 14:21:00 -0700

    added substring

---
+++ two_pointers/substring_question.py
@@ -0,0 +1,22 @@
+from typing import List
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def isSubsequence(s: str, t: str) -> bool:
+    pass
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    s = "abc"
+    t = "ahbgdc"
+    assert isSubsequence(s, t) == True
+
+    s = "axc"
+    t = "ahbgdc"
+    assert isSubsequence(s, t) == False
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

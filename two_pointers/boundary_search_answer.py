5bae4a554445e71d559ffda9b0b2b2473786b620
“Carlos <cmariscalaws@gmail.com>
2025-08-05 14:09:31 -0700

    added boundary search answer

--- two_pointers/boundary_search_answer.py
+++ two_pointers/boundary_search_answer.py
@@ -2,7 +2,19 @@
 from typing import List
 
 def find_boundary(arr: List[bool]) -> int:
-    pass
+    left, right = 0, len(arr) - 1
+    boundary_index = -1
+    
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == True:
+            boundary_index = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+    
+    return boundary_index
 
 if __name__ == '__main__':
     arr = [False, False, True, True, True]

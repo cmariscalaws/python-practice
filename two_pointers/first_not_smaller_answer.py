0b0821662ab4a928ed61c3eee3dd124c2f9986dc
“Carlos <cmariscalaws@gmail.com>
2025-08-06 13:51:47 -0700

    new binary search problem called first not smaller

--- two_pointers/first_not_smaller_answer.py
+++ two_pointers/first_not_smaller_answer.py
@@ -44,4 +44,25 @@ if __name__ == '__main__':
     print(f"nums:{nums} target:{target} expected:{expected} result: {result}")
     assert result == expected, f"Expected {expected}, but got {result}"
 
+    nums = [0]
+    target = 0
+    expected = 0
+    result = first_not_smaller(nums, target)
+    print(f"nums:{nums} target:{target} expected:{expected} result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
+
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    target = 10
+    expected = 9
+    result = first_not_smaller(nums, target)
+    print(f"nums:{nums} target:{target} expected:{expected} result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
+
+    nums = [1, 2, 2, 2, 2, 2, 2, 3, 5, 8, 8, 10]
+    target = 2
+    expected = 1
+    result = first_not_smaller(nums, target)
+    print(f"nums:{nums} target:{target} expected:{expected} result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
+
 

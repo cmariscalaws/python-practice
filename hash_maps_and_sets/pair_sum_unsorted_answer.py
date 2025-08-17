6f2719aaebb0396a60114b578f2f82ab316d0985
“Carlos <cmariscalaws@gmail.com>
2025-08-16 19:49:26 -0700

    added more test cases

--- hash_maps_and_sets/pair_sum_unsorted_answer.py
+++ hash_maps_and_sets/pair_sum_unsorted_answer.py
@@ -18,8 +18,22 @@ if __name__ == '__main__':
     target = 3
     expectedPair = [0,2]
     result = []
-
     result = pair_sum_unsorted(nums, target)
-
+    print(f"nums={nums} target={target} expected={expectedPair} result={result}")
+    assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
+    
+    nums = [-1, 3, 4, 2]
+    target = 1
+    expectedPair = [0,3]
+    result = []
+    result = pair_sum_unsorted(nums, target)
+    print(f"nums={nums} target={target} expected={expectedPair} result={result}")
+    assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
+    
+    nums = [-1, 3, 4, 2]
+    target = 8
+    expectedPair = []
+    result = []
+    result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
--- hash_maps_and_sets/pair_sum_unsorted_question.py
+++ hash_maps_and_sets/pair_sum_unsorted_question.py
@@ -2,15 +2,40 @@
 from typing import List
 
 def pair_sum_unsorted(nums: List[int], target: int) -> List[int]:
-    pass
+    num_index_map = {}
+    
+    for i, num in enumerate(nums):
+        
+        compliment = target - num
+        
+        if compliment in num_index_map:
+            return [num_index_map[compliment], i]
+        else:
+            num_index_map[num] = i
+            
+    return []
 
 if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 3
     expectedPair = [0,2]
     result = []
-
     result = pair_sum_unsorted(nums, target)
-
+    print(f"nums={nums} target={target} expected={expectedPair} result={result}")
+    assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
+    
+    nums = [-1, 3, 4, 2]
+    target = 1
+    expectedPair = [0,3]
+    result = []
+    result = pair_sum_unsorted(nums, target)
+    print(f"nums={nums} target={target} expected={expectedPair} result={result}")
+    assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
+    
+    nums = [-1, 3, 4, 2]
+    target = 8
+    expectedPair = []
+    result = []
+    result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"

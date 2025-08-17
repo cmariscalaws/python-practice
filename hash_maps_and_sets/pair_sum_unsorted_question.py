a2b11a4e781e8d18a577d83c29c5ae132243dc74
“Carlos <cmariscalaws@gmail.com>
2025-08-17 10:48:09 -0700

    removed answer from question

--- hash_maps_and_sets/pair_sum_unsorted_question.py
+++ hash_maps_and_sets/pair_sum_unsorted_question.py
@@ -2,18 +2,7 @@
 from typing import List
 
 def pair_sum_unsorted(nums: List[int], target: int) -> List[int]:
-    num_index_map = {}
-    
-    for i, num in enumerate(nums):
-        
-        compliment = target - num
-        
-        if compliment in num_index_map:
-            return [num_index_map[compliment], i]
-        else:
-            num_index_map[num] = i
-            
-    return []
+    pass
 
 if __name__ == '__main__':
     nums = [-1, 3, 4, 2]

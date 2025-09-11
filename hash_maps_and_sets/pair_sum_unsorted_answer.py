26d7446dde3812525e90f3e36097dc4a8c19bd6e
“Carlos <cmariscalaws@gmail.com>
2025-09-11 01:43:22 -0700

    fixed tests

--- hash_maps_and_sets/pair_sum_unsorted_answer.py
+++ hash_maps_and_sets/pair_sum_unsorted_answer.py
@@ -18,7 +18,6 @@ if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 3
     expectedPair = [0,2]
-    result = []
     result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
@@ -26,7 +25,6 @@ if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 1
     expectedPair = [0,3]
-    result = []
     result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
@@ -34,7 +32,6 @@ if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 8
     expectedPair = []
-    result = []
     result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
--- hash_maps_and_sets/pair_sum_unsorted_question.py
+++ hash_maps_and_sets/pair_sum_unsorted_question.py
@@ -2,13 +2,22 @@
 from typing import List
 
 def pair_sum_unsorted(nums: List[int], target: int) -> List[int]:
-    pass
+    num_index_dict = {}
+
+    for i, num in enumerate(nums):
+        compliment = target - num
+
+        if compliment in num_index_dict:
+            return [num_index_dict[compliment], i]
+        else:
+            num_index_dict[num] = i
+
+    return []        
 
 if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 3
     expectedPair = [0,2]
-    result = []
     result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
@@ -16,7 +25,6 @@ if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 1
     expectedPair = [0,3]
-    result = []
     result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"
@@ -24,7 +32,6 @@ if __name__ == '__main__':
     nums = [-1, 3, 4, 2]
     target = 8
     expectedPair = []
-    result = []
     result = pair_sum_unsorted(nums, target)
     print(f"nums={nums} target={target} expected={expectedPair} result={result}")
     assert result == expectedPair, f"Expected {expectedPair}, but got {result}"

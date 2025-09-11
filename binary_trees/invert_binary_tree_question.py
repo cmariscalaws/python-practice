cc2936d01062c3d2b36ca4d52481f738b66a76cf
“Carlos <cmariscalaws@gmail.com>
2025-09-11 01:22:48 -0700

    added printing of expected

--- binary_trees/invert_binary_tree_question.py
+++ binary_trees/invert_binary_tree_question.py
@@ -85,4 +85,5 @@ if __name__ == "__main__":
     print("Original:", tree.to_list())  # [1, 2, 3, 4, 5, 6, 7]
     tree.invert(tree.root)
     print("Inverted:", tree.to_list())  # [1, 3, 2, 7, 6, 5, 4]
+    print("Expected:", expected)
     assert tree.to_list() == expected, f"Expected {expected}, but got {tree.to_list()}"

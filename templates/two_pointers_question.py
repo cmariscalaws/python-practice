305ef9cc1663aed303735afbf51821dd0ffd40ea
“Carlos <cmariscalaws@gmail.com>
2025-09-13 12:22:13 -0700

    added test cases to question

--- templates/two_pointers_question.py
+++ templates/two_pointers_question.py
@@ -12,9 +12,37 @@ def pair_sum_sorted(nums: List[int], target: int) -> List[int]:
 if __name__ == '__main__':
     nums = [-5,-2, 3, 4, 6]
     expected = [2, 3]
-    result = pair_sum_sorted(nums, 7)
+    target = 7
+    result = pair_sum_sorted(nums, target)
+    print(f"nums: {nums}, target: {target}, expected: {expected}, result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
 
-    print(result)
+    nums = [-5,-2, 3, 4, 6]
+    expected = []
+    target = 12
+    result = pair_sum_sorted(nums, target)
+    print(f"nums: {nums}, target: {target}, expected: {expected}, result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
+
+    nums = []
+    expected = []
+    target = 7
+    result = pair_sum_sorted(nums, 7)
+    print(f"nums: {nums}, target: {target}, expected: {expected}, result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
+
+    nums = [-5,-2, 3, 4, 6]
+    expected = [3, 4]
+    target = 10
+    result = pair_sum_sorted(nums, target)
+    print(f"nums: {nums}, target: {target}, expected: {expected}, result: {result}")
+    assert result == expected, f"Expected {expected}, but got {result}"
+
+    nums = [-5,-2, 3, 4, 6]
+    expected = [0, 4]
+    target = 1
+    result = pair_sum_sorted(nums, target)
+    print(f"nums: {nums}, target: {target}, expected: {expected}, result: {result}")
     assert result == expected, f"Expected {expected}, but got {result}"
 
 

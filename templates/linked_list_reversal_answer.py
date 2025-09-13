0918bc711a592c9ad1c033f00c071d90efa255cd
“Carlos <cmariscalaws@gmail.com>
2025-09-13 12:14:03 -0700

    added test case for linked list

--- templates/linked_list_reversal_answer.py
+++ templates/linked_list_reversal_answer.py
@@ -58,8 +58,19 @@ def linked_list_reversal(head: ListNode) -> ListNode:
     return prev
 
 if __name__ == '__main__':
+    print("--- Test 1 ---")
     head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
     expectedResult = ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1)))))
+    new_head = linked_list_reversal(head)
+    print("result:")
+    print(new_head)
+    print("expected:")
+    print(expectedResult)
+    assert new_head == expectedResult, f"Expected {expectedResult}, but got {new_head}"
+
+    print("--- Test 2 ---")
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5, ListNode(6))))))
+    expectedResult = ListNode(6, ListNode(5, ListNode(4, ListNode(3, ListNode(2, ListNode(1))))))
     new_head = linked_list_reversal(head)
     print("result:")
     print(new_head)

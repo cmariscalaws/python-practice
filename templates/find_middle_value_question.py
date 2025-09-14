3a2ede1ce9f1c10a8b6dc93e50fbd68ec064145a
cmariscalaws <cmariscalaws@gmail.com>
2025-09-14 14:38:06 -0700

    fixed naming to not give away solution

--- templates/find_middle_value_question.py
+++ templates/find_middle_value_question.py
@@ -1,23 +1,23 @@
 from utils import ListNode
 
-def find_middle_node_slow_fast_technique(head: ListNode) -> ListNode:
+def find_middle_node(head: ListNode) -> ListNode:
     pass
 
 if __name__ == '__main__':
     head = ListNode(0, ListNode(1, ListNode(2, ListNode(3, ListNode(4)))))
     expected = 2
-    result = find_middle_node_slow_fast_technique(head)
+    result = find_middle_node(head)
     print(f"list={head}, expected={expected}, result={result}")
     assert result == expected, f"Expected {expected}, but got {result}"
 
     head = ListNode(0, ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
     expected = 3
-    result = find_middle_node_slow_fast_technique(head)
+    result = find_middle_node(head)
     print(f"list={head}, expected={expected}, result={result}")
     assert result == expected, f"Expected {expected}, but got {result}"
 
     head = ListNode(0, ListNode(1))
     expected = 1
-    result = find_middle_node_slow_fast_technique(head)
+    result = find_middle_node(head)
     print(f"list={head}, expected={expected}, result={result}")
     assert result == expected, f"Expected {expected}, but got {result}"
